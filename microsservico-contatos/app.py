512e93c3954b29981e49e8f9295a5a74b11671e8
Lucas Henrique Gonçalves Wodtke <lucas.wodtke@hotmail.com>
2025-06-18 11:40:26 -0300

    Update app.py

--- microsservico-contatos/app.py
+++ microsservico-contatos/app.py
@@ -13,17 +13,22 @@ def gerenciar_contatos():
             nome = dados['nome']
             categoria_str = dados['categoria'].upper()
             categoria = CategoriaContato[categoria_str]
+
             novo_contato = Contato(nome=nome, categoria=categoria)
+
             if 'telefones' in dados and isinstance(dados['telefones'], list):
                 for tel_data in dados['telefones']:
                     tipo_tel_str = tel_data['tipo'].upper()
                     tipo_tel = TipoTelefone[tipo_tel_str]
                     telefone = Telefone(numero=tel_data['numero'], tipo=tipo_tel)
                     novo_contato.adicionar_telefone(telefone)
+
             banco_de_dados_contatos[novo_contato.id] = novo_contato
             return jsonify(novo_contato.to_dict()), 201
+
         except (KeyError, ValueError) as e:
             return jsonify({"erro": f"Erro nos dados de entrada: {str(e)}"}), 400
+
     else:  # GET
         lista_contatos = [contato.to_dict() for contato in banco_de_dados_contatos.values()]
         return jsonify(lista_contatos), 200
